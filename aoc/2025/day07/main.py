b259f2b1d5d4593651e2b67ecc4d588cea2fb134
John Tan <john.v.tan@gmail.com>
2025-12-07 20:56:07 -0500

    aoc/2025: Day 07

---
+++ aoc/2025/day07/main.py
@@ -0,0 +1,72 @@
+with open("input.txt") as f:
+    lines = f.read().strip().split("\n")
+
+test_lines = """\
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............""".split()
+
+
+def p1(lines: list[str]) -> int:
+    beam_cols = set()
+    beam_cols.add(lines[0].find("S"))
+    nsplit = 0
+
+    for line in lines[1:]:
+        to_split = []
+        for beam in beam_cols:
+            if line[beam] != "^":
+                continue
+
+            nsplit += 1
+            to_split.append(beam)
+
+        for beam in to_split:
+            beam_cols.remove(beam)
+            beam_cols.add(beam - 1)
+            beam_cols.add(beam + 1)
+
+    return nsplit
+
+
+def p2(lines: list[str]) -> int:
+    num_cols = len(lines[0])
+    num_rows = len(lines)
+    cache = {}
+
+    def count_timelines_to(row: int, col: int) -> int:
+        if col < 0 or col > num_cols:
+            return 0
+
+        if row >= num_rows:
+            return 1
+
+        # todo: base case
+        if (row, col) not in cache:
+            if lines[row][col] == "^":
+                cache[(row, col)] = count_timelines_to(
+                    row, col - 1
+                ) + count_timelines_to(row, col + 1)
+            else:
+                cache[(row, col)] = count_timelines_to(row + 1, col)
+
+        return cache[(row, col)]
+
+    return count_timelines_to(1, lines[0].find("S"))
+
+
+print(p1(lines))
+print(p2(lines))
